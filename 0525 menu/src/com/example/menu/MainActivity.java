c0dadb63cf948afaac1ec316ca086d5834e01f8e
stoytoc <elisoyeon@gmail.com>
2014-05-25 22:31:36 +0900

    0525 menu

---
+++ 0525 menu/src/com/example/menu/MainActivity.java
@@ -0,0 +1,69 @@
+package com.example.menu;
+
+import android.app.Activity;
+import android.app.ActionBar;
+import android.app.Fragment;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+import android.os.Build;
+
+public class MainActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+    	menu.add(0, 0, 0, "Home").setIcon(android.R.drawable.ic_menu_revert);
+        menu.add(0, 1, 0, "Exit").setIcon(android.R.drawable.ic_menu_revert);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+        
+        Intent intent_home = new Intent(this, MainActivity.class);
+
+        switch(id) {
+        case 0: startActivity(intent_home); break;
+        case 1: System.exit(0); break;
+        default: break;
+        }
+
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
+    /**
+     * A placeholder fragment containing a simple view.
+     */
+    public static class PlaceholderFragment extends Fragment {
+
+        public PlaceholderFragment() {
+        }
+
+        @Override
+        public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                Bundle savedInstanceState) {
+            View rootView = inflater.inflate(R.layout.fragment_main, container, false);
+            return rootView;
+        }
+    }
+
+}
